bb791250809551e6790d814b6ff69cdcdcea4082
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-12-01 14:20:26 +0100

    added unexpected end to the game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -48,6 +48,8 @@ class GamesController < ApplicationController
         redirect_to game_path(game)
       else
         render plain: "You must enter unique set of colors."
+      end
+    end
     if session[:current_player_id]
       player = Player.find(session[:current_player_id])
       game = Game.create!(passcode: params[:passcode].upcase.split(" ").to_s)
