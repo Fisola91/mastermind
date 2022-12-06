fbf7a86640c7a4e0690a06d9988bbdfdab7134a4
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-12-06 11:48:36 +0100

    A player can play and guess two exact and one wrong colors on first attempt

--- spec/system/guessing_game_spec.rb
+++ spec/system/guessing_game_spec.rb
@@ -438,4 +438,33 @@ RSpec.describe "guessing game" do
 
     expect(page).to have_text("Two colors guessed, one at the exact position and one at the wrong position.")
   end
+
+  it "allows a player to play and guess two exact and one wrong color on first attempt" do
+    visit "/"
+    click_on "codebreaker"
+    fill_in "player name", with: "Tester"
+    click_on "Start player session"
+    expect(page).to have_text("You are playing as Tester")
+    click_on "codebreaker"
+    expect(page).to have_text("Guess!")
+    expect(page).to have_text("Attempt: 1/4")
+    expect(page).to have_text("Your guess")
+
+    passcode_colors = JSON.parse(Game.last.passcode)
+    select_values = passcode_colors.map(&:downcase)
+
+    valid_colors = ValidColor.select(:colors).first[:colors]
+    select_colors = valid_colors.map(&:downcase)
+
+    color_difference = select_colors - select_values
+
+    select select_values[0], from: "color-1"
+    select select_values[1], from: "color-2"
+    select select_values[3], from: "color-3"
+    select color_difference[1], from: "color-4"
+
+    click_on "Check"
+
+    expect(page).to have_text("Three colors guessed, two at the exact position and one at the wrong position.")
+  end
 end
