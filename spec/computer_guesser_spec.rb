2b8054d954c7016b7842aed8cf9932f558f0623e
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-12-16 20:19:18 +0100

    Revert "computer guess on with different feedbacks"
    This reverts commit 51bb384caf3a1b56542965f1f4f25fe5b7e11617.

--- spec/computer_guesser_spec.rb
+++ spec/computer_guesser_spec.rb
@@ -6,6 +6,7 @@ VALID_COLORS = %w(RED ORANGE YELLOW GREEN BLUE PURPLE)
 RSpec.describe ComputerGuesser do
 
   subject { described_class.new(previous_guesses)}
+  let(:passcode) { ["GREEN", "RED", "PURPLE", "BLUE"] }
 
   context "when one match on first try" do
     let(:previous_guesses) do
@@ -18,7 +19,7 @@ RSpec.describe ComputerGuesser do
     end
 
     it "tries to confirm first is the exact, tries two new colors, moves second color to last" do
-      next_guess = subject.guess
+      next_guess = subject.guess(passcode)
       expect(next_guess).to eq(["GREEN", "RED", "YELLOW", "ORANGE"])
     end
   end
@@ -37,8 +38,8 @@ RSpec.describe ComputerGuesser do
       ]
     end
 
-    it "tries to confirm two exact colors, one new color and move third color to last" do
-      next_guess = subject.guess
+    it "tries to confirm three exact colors and one new colors" do
+      next_guess = subject.guess(passcode)
       expect(next_guess).to eq(["GREEN", "RED", "PURPLE", "YELLOW"])
     end
   end
@@ -61,70 +62,9 @@ RSpec.describe ComputerGuesser do
       ]
     end
 
-
-    it "tries to confirm three exact colors and one new color" do
-      next_guess = subject.guess
-      expect(next_guess).to eq(["GREEN", "RED", "PURPLE", "BLUE"])
-    end
-  end
-
-  context "when four exact match on third try" do
-    let(:previous_guesses) do
-      [
-        AttemptWithFeedback.new(
-          guess: ["GREEN", "ORANGE", "BLUE", "YELLOW"],
-          feedback: [:exact]
-        ),
-        AttemptWithFeedback.new(
-          guess: ["GREEN", "RED", "YELLOW", "ORANGE"],
-          feedback: [:exact, :exact]
-        ),
-        AttemptWithFeedback.new(
-          guess: ["GREEN", "RED", "PURPLE", "ORANGE"],
-          feedback: [:exact, :exact, :exact, :exact]
-        )
-      ]
-    end
-
     it "tries to confirm four exact colors" do
-      next_guess = subject.guess
-      expect(next_guess).to eq(["GREEN", "RED", "PURPLE", "ORANGE"])
-    end
-  end
-
-  context "when no match on first try" do
-    let(:previous_guesses) do
-      [
-        AttemptWithFeedback.new(
-          guess: ["BLUE", "BLUE", "ORANGE", "ORANGE"],
-          feedback: []
-        )
-      ]
-    end
-
-    it "tries to confirm no match with the previous color" do
-      next_guess = subject.guess
-      expect(next_guess).to eq(["RED", "RED", "YELLOW", "YELLOW"])
-    end
-  end
-
-  context "when two exact and two partial match on second try" do
-    let(:previous_guesses) do
-      [
-        AttemptWithFeedback.new(
-          guess: ["BLUE", "BLUE", "ORANGE", "ORANGE"],
-          feedback: []
-        ),
-        AttemptWithFeedback.new(
-          guess: ["RED", "RED", "YELLOW", "YELLOW"],
-          feedback: [:exact, :exact, :partial, :partial]
-        )
-      ]
-    end
-
-    it "tries to confirm two exact and partial colors" do
-      next_guess = subject.guess
-      expect(next_guess).to eq(["RED", "YELLOW", "PURPLE", "GREEN"])
+      next_guess = subject.guess(passcode)
+      expect(next_guess).to eq(["GREEN", "RED", "PURPLE", "BLUE"])
     end
   end
 end
