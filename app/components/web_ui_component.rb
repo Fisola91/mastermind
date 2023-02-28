ded64b2fa709fbf7e0009753753827571bb0e619
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-02-28 14:38:46 +0100

    test passed for rendering game_board components

--- app/components/web_ui_component.rb
+++ app/components/web_ui_component.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class WebUiComponent < ViewComponent::Base
   def guess_attempts
     10
--- spec/components/game_board_component_spec.rb
+++ spec/components/game_board_component_spec.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
 
 require "rails_helper"
 
 RSpec.describe GameBoardComponent, type: :component do
-  #pending "add some examples to (or delete) #{__FILE__}"
   let(:game) {
     Game.new(passcode: %w(red red green green).to_json)
   }
