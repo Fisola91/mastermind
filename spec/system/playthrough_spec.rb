232bc6f36968b23fc0138e789653517def12c6cc
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-03-10 10:42:08 +0100

    A player can get a lost message after exhausting the chances with wrong color combination

--- spec/system/playthrough_spec.rb
+++ spec/system/playthrough_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "signed user plays a game" do
+  let(:game) {
+    Game.new(id: 1, passcode:%w(RED ORANGE YELLOW GREEN).to_json)
+  }
+
+  let(:attempts) {
+    [
+      Attempt.new(values: %w(RED ORANGE YELLOW BLUE))
+    ]
+  }
+
   it "can log in, and win the game" do
     allow(ValidColors).to receive(:four_random_colors)
       .and_return(%w(RED ORANGE YELLOW GREEN))
@@ -43,5 +53,55 @@ RSpec.describe "signed user plays a game" do
     expect(find(".board")).to have_text("Congratulations!")
 
     expect(find(".guess-row[data-number='1']")).to have_css(".guess-rating.bg-green", count: 4)
+  end
+
+  it "can log in, and loose the game" do
+    allow(ValidColors).to receive(:four_random_colors)
+      .and_return(%w(RED ORANGE YELLOW GREEN))
+
+    allow_any_instance_of(GameBoardComponent).to receive(:guess_attempts).and_return(1)
+
+    object = GameBoardComponent.new(
+      game: game,
+      attempts: attempts)
+
+    visit "/"
+    fill_in "Player name", with: "Tester"
+    click_on "Start player session"
+    expect(page).to have_text "You are playing as Tester"
+    expect(page).to have_button "Sign out"
+    click_on "New game"
+    expect(page).to have_css(".board")
+
+
+    find(".color-picker div[data-color=red]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+
+    find(".color-picker div[data-color=orange]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+
+    find(".color-picker div[data-color=yellow]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+    expect(find(".current-attempt")).to have_selector(".bg-yellow[data-number='2']")
+
+    find(".color-picker div[data-color=blue]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+    expect(find(".current-attempt")).to have_selector(".bg-yellow[data-number='2']")
+    expect(find(".current-attempt")).to have_selector(".bg-blue[data-number='3']")
+
+    find(".color-picker div[data-color=blue]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+    expect(find(".current-attempt")).to have_selector(".bg-yellow[data-number='2']")
+    expect(find(".current-attempt")).to have_selector(".bg-blue[data-number='3']")
+
+    click_on "Check"
+
+    expect(find(".board")).to have_content("Sorry, you've lost! It was a bit difficult")
+
+    expect(find(".guess-row[data-number='1']")).to have_css(".guess-rating.bg-green", count: 3)
   end
 end
