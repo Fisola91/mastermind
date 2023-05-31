7ec214e6ada60b13eb294485e3eb3e19b2faaf8c
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-05-31 21:51:09 +0200

    unserialized the ValidColor array

--- app/models/valid_color.rb
+++ app/models/valid_color.rb
@@ -3,8 +3,4 @@ class ValidColor < ApplicationRecord
                         .first[:colors]
                         .shuffle
                         .take(4) }
-
-  after_initialize do |b|
-    b.colors = [] if b.colors == nil
-  end
 end
