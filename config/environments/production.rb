2555e16dc977c199952e0905b3d915532d5ce679
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-06-06 12:29:38 +0200

    Added a debug code to the production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-
+  config.log_level = :debug
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
