20a77e5b3e5365e71af14b2eb01a030554ff537a
Fisola <81283781+Fisola91@users.noreply.github.com>
2023-06-07 08:59:50 +0200

    Code debug (#70)
    * Added a rescue to the passcode method to capture the error message
    
    * Added log statement to debug codes in production
    
    * Added log statement to debug codes in production
    
    * Removed require fly_logger from the production file
    
    * Removed require fly_logger from the production file
    
    * Removed require fly_logger from the production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  config.logger.extend(ActiveSupport::Logger.broadcast(Fly.logger)) 
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
