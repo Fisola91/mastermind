67047ddea1ea1fc7057135e72c5f53d53cb55d2c
Fisola <81283781+Fisola91@users.noreply.github.com>
2023-06-09 12:36:28 +0200

    Code debug (#77)
    * Added a rescue to the passcode method to capture the error message
    
    * Added log statement to debug codes in production
    
    * Added log statement to debug codes in production
    
    * Removed require fly_logger from the production file
    
    * Removed require fly_logger from the production file
    
    * Removed require fly_logger from the production file
    
    * Added a debug code to print out error info on application logs
    
    * Serialized the game passcode and the attempt values
    
    * Serialized the game passcode and the attempt values
    
    * Updated the schema by making a default array for the game passcode
    
    * Removed after the initialize block from both the  game and attempt model
    
    * Disabled all  colors after the player won the game

--- spec/system/playthrough_spec.rb
+++ spec/system/playthrough_spec.rb
@@ -151,4 +151,53 @@ RSpec.describe "signed user plays a game" do
     expect(page).to have_button("Check", disabled: true)
     expect(page).not_to have_content("Invalid attempt: contains unknown color")
   end
+
+  it "can log in, win the game an disabled all colors" do
+    allow(ValidColors).to receive(:four_random_colors)
+      .and_return(%w(RED ORANGE YELLOW GREEN))
+
+    visit "/"
+    fill_in "Player name", with: "Tester"
+    click_on "Start player session"
+    expect(page).to have_text "You are playing as Tester"
+    expect(page).to have_button "Sign out"
+    click_on "New game"
+    expect(page).to have_css(".board")
+
+
+    find(".color-picker div[data-color=red]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+
+    find(".color-picker div[data-color=orange]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+
+    find(".color-picker div[data-color=yellow]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+    expect(find(".current-attempt")).to have_selector(".bg-yellow[data-number='2']")
+
+    find(".color-picker div[data-color=green]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+    expect(find(".current-attempt")).to have_selector(".bg-yellow[data-number='2']")
+    expect(find(".current-attempt")).to have_selector(".bg-green[data-number='3']")
+
+    find(".color-picker div[data-color=blue]").click
+    expect(find(".current-attempt")).to have_selector(".bg-red[data-number='0']")
+    expect(find(".current-attempt")).to have_selector(".bg-orange[data-number='1']")
+    expect(find(".current-attempt")).to have_selector(".bg-yellow[data-number='2']")
+    expect(find(".current-attempt")).to have_selector(".bg-green[data-number='3']")
+
+    click_on "Check"
+
+    expect(find(".board")).to have_text("Congratulations!")
+
+    expect(find(".guess-row[data-number='1']")).to have_css(".guess-rating.bg-green", count: 4)
+    
+    find(".color-picker div[data-color=red]").click
+
+    expect(find(".current-attempt")).not_to have_selector(".bg-red[data-number='0']")
+    expect(page).not_to have_content("Invalid attempt: contains unknown color")
+  end
 end
