d2c95988fbc0dd0bfb788a364690244013db845c
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-02-27 15:29:32 +0100

    Test passed: User can play the game

--- spec/system/playthrough_spec.rb
+++ spec/system/playthrough_spec.rb
@@ -194,7 +194,7 @@ RSpec.describe "signed user plays a game" do
     expect(find(".board")).to have_text("Congratulations!")
 
     expect(find(".guess-row[data-number='1']")).to have_css(".guess-rating.bg-green", count: 4)
-    
+
     find(".color-picker div[data-color=red]").click
     find(".color-picker div[data-color=orange]").click
     find(".color-picker div[data-color=yellow]").click
